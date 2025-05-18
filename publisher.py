290da6cda5913d48fe3d826b4d23ea0a6061fd94
Tallya Barbosa <ayllat2001@gmail.com>
2025-05-18 09:48:25 -0300

    add new topics

--- publisher.py
+++ publisher.py
@@ -1,11 +1,27 @@
 import zmq, time
 from constPS import * #-
+import random
 
 context = zmq.Context()
 s = context.socket(zmq.PUB)        # create a publisher socket
 p = "tcp://"+HOST+":"+ PORT      # how and where to communicate
 s.bind(p)                          # bind socket to the address
+
+topics = ['TIME', 'RPS', 'COIN']
+rps_values = ['rock', 'paper', 'scissors'] # simulated rock/paper/scissors game
+coin_flip_values = ['heads', 'tails'] # simulated coin flip game
+
 while True:
-	time.sleep(5)                    # wait every 5 seconds
-	msg = str.encode("TIME " + time.asctime())
-	s.send(msg) # publish the current time
+	time.sleep(3)                    # wait every 3 seconds
+	topic = random.choice(topics)
+	if topic == 'TIME':
+		msg = str.encode("TIME " + time.asctime())
+		s.send(msg) # publish the current time
+	
+	if topic == 'RPS':
+		msg = str.encode("RPS " + ''.join(random.choices(rps_values, k=2)))
+		s.send(msg)
+
+	if topic == 'COIN':
+		msg = str.encode("COIN " + random.choice(coin_flip_values))
+		s.send(msg)
--- subscriber.py
+++ subscriber.py
@@ -6,7 +6,9 @@ s = context.socket(zmq.SUB)          # create a subscriber socket
 p = "tcp://"+ HOST +":"+ PORT        # how and where to communicate
 s.connect(p)                         # connect to the server
 s.setsockopt_string(zmq.SUBSCRIBE, "TIME")  # subscribe to TIME messages
+s.setsockopt_string(zmq.SUBSCRIBE, "RPS")
+s.setsockopt_string(zmq.SUBSCRIBE, "COIN")
 
-for i in range(5):  # Five iterations
-	time = s.recv()   # receive a message
-	print (bytes.decode(time))
+for i in range(10):  # Ten iterations
+	msg = s.recv()   # receive a message
+	print (bytes.decode(msg))
